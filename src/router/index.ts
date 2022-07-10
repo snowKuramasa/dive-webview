a78440fe76e593a238a626c066a72bc98c8816fd
kuramasa <kuramasa.green@gmail.com>
2022-07-10 20:25:16 +0900

    add sample websocket function

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
+import WebSocket from "@/views/WebSocket.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/WebSocket",
+    name: "WebSocket",
+    component: WebSocket,
   },
   {
     path: "/about",
